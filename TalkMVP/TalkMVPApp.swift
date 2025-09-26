a03dfa1edbad2fcd24202ad587c2815760e8ba75
𝄃𝄃𝄃𝄂𝄂𝄀𝄁𝄃𝄂𝄂𝄃 <dsong90@gmail.com>
2025-09-26 08:47:29 -0400

    Initial Commit

---
+++ TalkMVP/TalkMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkMVPApp.swift
+//  TalkMVP
+//
+//  Created by David Song on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TalkMVPTests/TalkMVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalkMVPTests.swift
+//  TalkMVPTests
+//
+//  Created by David Song on 9/26/25.
+//
+
+import Testing
+@testable import TalkMVP
+
+struct TalkMVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
